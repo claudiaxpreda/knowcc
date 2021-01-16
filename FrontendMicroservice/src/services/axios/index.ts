30e60152069a30d33e6f3633d0153f898ba316f6
claudia.preda <claudia.prd23@gmail.com>
2021-01-16 18:21:31 +0200

    Resolve env var frontend

--- FrontendMicroservice/src/services/axios/index.ts
+++ FrontendMicroservice/src/services/axios/index.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const GATEWAY_SERVICE_HOST = process.env.GATEWAY_SERVICE_HOST || 'localhost'
+const GATEWAY_SERVICE_HOST = process.env.REACT_APP_GATEWAY_SERVICE_HOST || 'localhost'
+console.log(process.env.REACT_APP_GATEWAY_SERVICE_HOST)
 
 export default axios.create({
   baseURL: `http://${GATEWAY_SERVICE_HOST}:3000`,
