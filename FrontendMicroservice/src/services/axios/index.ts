b76ddb069bdfa10ee11f26f168bd5eb4aab42fc1
claudia.preda <claudia.prd23@gmail.com>
2021-01-17 18:18:57 +0200

    Add nginx

--- FrontendMicroservice/src/services/axios/index.ts
+++ FrontendMicroservice/src/services/axios/index.ts
@@ -4,6 +4,6 @@ const GATEWAY_SERVICE_HOST = process.env.REACT_APP_GATEWAY_SERVICE_HOST || 'loca
 console.log(process.env.REACT_APP_GATEWAY_SERVICE_HOST)
 
 export default axios.create({
-  baseURL: `http://${GATEWAY_SERVICE_HOST}:80`,
+  baseURL: `http://${GATEWAY_SERVICE_HOST}:3000`,
   timeout: 2000
 })
--- docker-machine-setup.sh
+++ docker-machine-setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-docker-machine scp Database/init-db.sql $1:Database
+docker-machine scp $1 "mkdir Database"
+docker-machine scp Database/init-db.sql $1:Database/
 docker-machine scp -r kong/ $1:.
+docker-machine scp -r nginx/ $1:.
 docker-machine scp $2 $1:.
+docker-machine scp -r secrets/ $1:.
+docker-machine scp stack-portainer.yml $1:.
